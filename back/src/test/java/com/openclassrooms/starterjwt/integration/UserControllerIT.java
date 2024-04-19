34f4144fcfba90a5ab44d9f759f81587e301e48c
Chrystal Bastid <chrystal.bastid@gmail.com>
2024-04-19 19:20:33 +0200

    Création d'un fichier de test d'intégration + début des tests pour user controller

--- back/src/test/java/com/openclassrooms/starterjwt/integration/UserControllerIT.java
+++ back/src/test/java/com/openclassrooms/starterjwt/integration/UserControllerIT.java
@@ -12,6 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,29 +31,120 @@ public class UserControllerIT {
     private User testUser;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         testUser = User.builder()
                 .email("test@test.com")
                 .password("azerty")
                 .lastName("Test")
                 .firstName("ReTest")
+                .admin(false)
                 .build();
     }
 
     @AfterEach
-    void clean(){
+    void clean() {
         userRepository.deleteAll();
     }
 
     @Test
-    @WithMockUser(roles="USER")
-    @DisplayName("Obtenir un utilisateur par ID retourne un utilisateur valide")
+    @WithMockUser(roles = "USER")
+    @DisplayName("Obtenir un utilisateur par ID et retourne un utilisateur valide")
     public void testGetUserByIdFindsValidUser() throws Exception {
         //Given
-        Long testUserId=userRepository.save(testUser).getId();
+        Long testUserId = userRepository.save(testUser).getId();
 
         this.mockMvc.perform(get("/api/user/" + testUserId))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.lastName").value("Test"));
+    }
+
+    @Test
+    @DisplayName("Demande non autorisée pour supprimer un utilisateur par ID")
+    public void testUnauthorizedUserDeletionById() throws Exception {
+        //Given
+        Long testUserId = userRepository.save(testUser).getId();
+
+        //When
+        this.mockMvc.perform(delete("/api/user/" + testUserId))
+
+                //Then
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("Suppression non autorisée d'utilisateur par ID avec demandeur invalide")
+    public void testUnauthorizedUserDeletionByIdInvalidRequester() throws Exception {
+        //Given
+        Long testUserId = userRepository.save(testUser).getId();
+
+        //When
+        this.mockMvc.perform(delete("/api/user/" + testUserId).with(user("azerty@mail.com")))
+
+                //Then
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("Suppression autorisé d'utilisateur par ID pour utilisateur valide")
+    public void testValidUserDeletionById() throws Exception {
+        //Given
+        Long testUserId = userRepository.save(testUser).getId();
+
+        //When
+        this.mockMvc.perform(delete("/api/user/" + testUserId).with(user("test@test.com")))
+
+                //Then
+                .andExpect(status().isOk());
+        assertThat(userRepository.findByEmail("test@test.com").isPresent()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Suppression d'utilisateur non existant par utilisateur auth - BadRequest")
+    public void testDeleteInexistentUserIsBadRequest() throws Exception {
+        //Given
+
+        //When
+        this.mockMvc.perform(delete("/api/user/3A-1e").with(user("test@test.com")))
+
+                //Then
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Obtention d'utilisateur non existant - BadRequest")
+    public void testGetInexistentUserIsBadRequest() throws Exception {
+        //Given
+
+        //When
+        this.mockMvc.perform(get("/api/user/aZerTy").with(user("test@test.com")))
+
+                //Then
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Obtention d'utilisateur inexistant - NotFound")
+    public void testGetNonexistentUserIsNotFound() throws Exception {
+        //Given
+        long testUserId = userRepository.save(testUser).getId()+1;
+
+        //When
+        this.mockMvc.perform(get("/api/user/"+testUserId).with(user("test@test.com")))
+
+                //Then
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Suppression d'utilisateur non trouvé par utilisateur auth")
+    public void testDeleteNonexistentUserIsNotFound() throws Exception {
+        //Given
+        long testUserId=userRepository.save(testUser).getId()+1;
+
+        //When
+        this.mockMvc.perform(delete("/api/user/"+testUserId).with(user("test@test.com")))
+
+                //Then
+                .andExpect(status().isNotFound());
     }
 }
